dd811bfed0d368e0828be8471a758060de41f5e1
Corey Mckeel <cmckeel@codingdojo.com>
2023-09-27 18:21:36 -0400

    Ready for lecture

---
+++ week1/day2/strings.js
@@ -0,0 +1,33 @@
+let result = window.prompt("Enter your string");
+
+// const REGEX = /^[a-z ,.'-]+$/i
+// if (!result.match(REGEX)) alert("Improper format");
+
+document.getElementById("result").innerText = result;
+
+
+
+// Receiving data -> Everyone has their preferred way of typing
+/*
+
+    * Corey Mckeel
+    * Corey McKeel
+    * corey mckeel
+    * Corey mckeel
+    * coreymckeel
+    
+*/
+
+// Formatting
+
+/*
+
+    * Phone numbers
+    * Credit card numbers
+    * Address
+    * Dates
+    
+*/
+
+
+
