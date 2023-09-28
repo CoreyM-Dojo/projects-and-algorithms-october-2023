f682e1f0f97a869f764b8be1e44cbd99092a15bb
Corey Mckeel <cmckeel@codingdojo.com>
2023-09-28 18:08:47 -0400

    Ready for arrays lecture

--- week1/day3/arrays.js
+++ week1/day3/arrays.js
@@ -1,3 +1,31 @@
+let exampleArray = [];
+const objectArray = [];
+
+document.getElementById("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e.target[0].value);
+
+    const newArray = e.target[0].value.split(",");
+    exampleArray.push(newArray[0]);
+    console.log(exampleArray)
+})
+
+const objForm = document.forms[1]
+
+objForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newObj = {
+        name: objForm["name"].value,
+        favFood: objForm["favFood"].value,
+        favStack: objForm["favStack"].value
+
+    }
+    objectArray.push(newObj);
+    console.log(objectArray);
+})
+
+
+
 // Anatomy of arrays
 
 
@@ -5,13 +33,23 @@
 // push - add to end of an array
 
 
+// array.push(val)
+// What is actually happening?
+exampleArray.with(-1,20)
+console.log(exampleArray);
+
+
+
 
 // Get a value from an array
 // Arrays are indexed starting with 0
-
+console.log(exampleArray[2]);
 
 
 // pop
+// exampleArray.pop() // removes AND returns the last value in an array
+
+
 
 
 
