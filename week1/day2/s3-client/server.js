4d2ab049ad5aa1aa6f68c6069928c7599db26e9f
Corey <coreyt.mckeel@gmail.com>
2023-09-27 22:02:54 -0400

    implemented dotenv for access keys

--- week1/day2/s3-client/server.js
+++ week1/day2/s3-client/server.js
@@ -1,5 +1,6 @@
 const AWS = require('aws-sdk');
 const AXIOS = require('axios');
+require('dotenv').config();
 
 // Use the aws script: <script src="https://cdnjs.cloudflare.com/ajax/libs/aws-sdk/2.1043.0/aws-sdk.min.js"></script>
 
@@ -14,13 +15,11 @@ const AXIOS = require('axios');
 //     }
 // });
 
-// Access Key - AKIAVLBUZ26YTLIXOBHI
-// Secret Access Key - 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
 
 const S3 = new AWS.S3({
     region:"us-east-2",
-    accessKeyId: "AKIAVLBUZ26YTLIXOBHI",
-    secretAccessKey: "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"
+    accessKeyId: process.env.S3ACCESSKEY,
+    secretAccessKey: process.env.S3SECRET
 })
 
 const params = {
