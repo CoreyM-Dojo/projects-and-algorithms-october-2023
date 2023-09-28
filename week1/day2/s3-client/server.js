1702da792c7e0326bb8c0255b2406ae48af00881
Corey <coreyt.mckeel@gmail.com>
2023-09-28 18:47:03 -0400

    successfully retrieves object from s3 bucket

--- week1/day2/s3-client/server.js
+++ week1/day2/s3-client/server.js
@@ -1,48 +1,48 @@
-const AWS = require('aws-sdk');
-const AXIOS = require('axios');
-require('dotenv').config();
+const AWS = require("aws-sdk");
+const AXIOS = require("axios");
+const EXPRESS = require('express')
+const app = new EXPRESS();
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+require("dotenv").config();
 
 // Use the aws script: <script src="https://cdnjs.cloudflare.com/ajax/libs/aws-sdk/2.1043.0/aws-sdk.min.js"></script>
 
 // Then use this to get the information
 
-// s3.getObject(params, function(err, data) {
-//     if (err) console.log(err, err.stack);
-//     else {
-//         const blob = new Blob([data.Body], { type: data.ContentType });
-//         const imageUrl = URL.createObjectURL(blob);
-//         document.getElementById('yourImageElementId').src = imageUrl;
-//     }
-// });
-
-
 const S3 = new AWS.S3({
-    region:"us-east-2",
-    accessKeyId: process.env.S3ACCESSKEY,
-    secretAccessKey: process.env.S3SECRET
-})
+	region: "us-east-2",
+	accessKeyId: process.env.S3ACCESSKEY,
+	secretAccessKey: process.env.S3SECRET,
+});
 
 const params = {
-    Bucket: "officehours3",
-    Key: "burning-town.png"
-}
-
-
+	Bucket: "officehours3",
+	Key: "burning-town.png",
+};
 
 const toBase64 = async () => {
-    let imageBytes = await S3.getObject(params,(err, data) => {
+	let imageBytes = await S3.getObject(params, function (err, data) {
+		if (err) console.log(err, err.stack);
+		else {
+            console.log(data);
+			let image = Buffer.from(data.Body).toString('base64');
+            image = "data:"+"image/png"+";base64,"+image;
+            app.get("", async (req,res) => {
+                
+                res.render(__dirname + "/public/index.html", {image:image});
+            })
+		}
+	});
+    return imageBytes;
+};
+toBase64();
 
-        if (err)  {
-            console.log(err, err.stack);
-        } else     {return data.Body }// an error occurred
-    })
 
-    await console.log(imageBytes);
 
-        return await btoa(imageBytes.reduce((data, byte) => data + String.fromCharCode(byte), ''))
-}
 
-console.log(toBase64());
 
+
+app.listen(8000, () => console.log("Running on port 8000"));
 
 
